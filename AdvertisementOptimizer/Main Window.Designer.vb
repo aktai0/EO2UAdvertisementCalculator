fb187fa8e44a2231e743d4e8bf4fcd6e3a146896
aktai0 <xtoken.x@gmail.com>
2015-10-10 03:23:04 -0700

    Added beautiful icon

--- AdvertisementOptimizer/Main Window.Designer.vb
+++ AdvertisementOptimizer/Main Window.Designer.vb
@@ -22,6 +22,7 @@ Partial Class MainWindow
    'Do not modify it using the code editor.
    <System.Diagnostics.DebuggerStepThrough()> _
    Private Sub InitializeComponent()
+      Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainWindow))
       Me.WardGroup1 = New AdvertisementOptimizer.WardGroup()
       Me.AboutButton = New System.Windows.Forms.Button()
       Me.SuspendLayout()
@@ -54,6 +55,7 @@ Partial Class MainWindow
       Me.Controls.Add(Me.AboutButton)
       Me.Controls.Add(Me.WardGroup1)
       Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+      Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
       Me.MaximizeBox = False
       Me.MinimizeBox = False
       Me.Name = "MainWindow"
